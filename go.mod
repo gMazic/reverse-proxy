631fbcd544403d7981b9efefe9c0e4319e675ec7
anu1601cs <anuragvns1999@gmail.com>
2020-05-29 14:40:38 +0530

    add reverse proxy

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gMazic/reverse-proxy
 
 go 1.14
+
+require github.com/gorilla/mux v1.7.4
--- main.go
+++ main.go
@@ -1,160 +1,102 @@
 package main
 
 import (
-	"bytes"
+	"crypto/tls"
 	"encoding/json"
-	"io/ioutil"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-/*
-	Structs
-*/
+var configFile = flag.String("conf", "config.json", "configuration file")
+var httpAddress = flag.String("http", ":80", "http address")
+var httpsAddress = flag.String("https", ":8080", "https address")
+var httpsEnabled = flag.Bool("https-enabled", false, "enable https server")
+var verbose = flag.Bool("verbose", false, "explain what is being done")
 
-type requestPayloadStruct struct {
-	ProxyCondition string `json:"proxy_condition"`
+var config map[string]interface{}
+
+// NewReverseProxy New Reverse proxy
+func NewReverseProxy(scheme, host string) *httputil.ReverseProxy {
+	return httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: scheme,
+		Host:   host,
+	})
 }
 
-/*
-	Utilities
-*/
-
-// Get env var or default
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+// Register url
+func Register(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if *verbose {
+			log.Printf("request %s%s", r.RemoteAddr, r.RequestURI)
+		}
+		// w.Header().Set("Access-Control-Allow-Origin", "*")
+		// w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+		p.ServeHTTP(w, r)
 	}
-	return fallback
 }
-
-/*
-	Getters
-*/
-
-// Get the port to listen on
-func getListenAddress() string {
-	port := getEnv("PORT", "1338")
-	return ":" + port
-}
-
-// Get the url for a given proxy condition
-func getProxyUrl(proxyConditionRaw string) string {
-	proxyCondition := strings.ToUpper(proxyConditionRaw)
-
-	a_condtion_url := os.Getenv("A_CONDITION_URL")
-	b_condtion_url := os.Getenv("B_CONDITION_URL")
-	default_condtion_url := os.Getenv("DEFAULT_CONDITION_URL")
-
-	if proxyCondition == "A" {
-		return a_condtion_url
-	}
-
-	if proxyCondition == "B" {
-		return b_condtion_url
-	}
-
-	return default_condtion_url
-}
-
-/*
-	Logging
-*/
-
-// Log the typeform payload and redirect url
-func logRequestPayload(requestionPayload requestPayloadStruct, proxyUrl string) {
-	log.Printf("proxy_condition: %s, proxy_url: %s\n", requestionPayload.ProxyCondition, proxyUrl)
-}
-
-// Log the env variables required for a reverse proxy
-func logSetup() {
-	a_condtion_url := os.Getenv("A_CONDITION_URL")
-	b_condtion_url := os.Getenv("B_CONDITION_URL")
-	default_condtion_url := os.Getenv("DEFAULT_CONDITION_URL")
-
-	log.Printf("Server will run on: %s\n", getListenAddress())
-	log.Printf("Redirecting to A url: %s\n", a_condtion_url)
-	log.Printf("Redirecting to B url: %s\n", b_condtion_url)
-	log.Printf("Redirecting to Default url: %s\n", default_condtion_url)
-}
-
-/*
-	Reverse Proxy Logic
-*/
-
-// Serve a reverse proxy for a given url
-func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
-	// parse the url
-	url, _ := url.Parse(target)
-
-	// create the reverse proxy
-	proxy := httputil.NewSingleHostReverseProxy(url)
-
-	// Update the headers to allow for SSL redirection
-	req.URL.Host = url.Host
-	req.URL.Scheme = url.Scheme
-	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
-	req.Host = url.Host
-
-	// Note that ServeHttp is non blocking and uses a go routine under the hood
-	proxy.ServeHTTP(res, req)
-}
-
-// Get a json decoder for a given requests body
-func requestBodyDecoder(request *http.Request) *json.Decoder {
-	// Read body to buffer
-	body, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		log.Printf("Error reading body: %v", err)
-		panic(err)
-	}
-
-	// Because go lang is a pain in the ass if you read the body then any susequent calls
-	// are unable to read the body again....
-	request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
-	return json.NewDecoder(ioutil.NopCloser(bytes.NewBuffer(body)))
-}
-
-// Parse the requests body
-func parseRequestBody(request *http.Request) requestPayloadStruct {
-	decoder := requestBodyDecoder(request)
-
-	var requestPayload requestPayloadStruct
-	err := decoder.Decode(&requestPayload)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return requestPayload
-}
-
-// Given a request send it to the appropriate url
-func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
-	requestPayload := parseRequestBody(req)
-	url := getProxyUrl(requestPayload.ProxyCondition)
-
-	logRequestPayload(requestPayload, url)
-
-	serveReverseProxy(url, res, req)
-}
-
-/*
-	Entry
-*/
 
 func main() {
-	// Log setup values
-	logSetup()
+	flag.Usage = func() {
+		fmt.Printf("usage: %s [options]\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	// start server
-	http.HandleFunc("/", handleRequestAndRedirect)
-	if err := http.ListenAndServe(getListenAddress(), nil); err != nil {
-		panic(err)
+	folder, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	file, err := os.Open(filepath.Join(folder, *configFile))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		log.Fatalln(err)
+	}
+
+	for path, host := range config["routes"].(map[string]interface{}) {
+		log.Printf("%s -> %s", path, host)
+		if strings.HasPrefix(path, "#") {
+			// skip comments
+			continue
+		}
+		u, err := url.Parse(host.(string))
+		if err != nil {
+			// skip invalid hosts
+			log.Println(err)
+			continue
+		}
+		if u.Scheme == "https" && !*httpsEnabled {
+			log.Println("https scheme detected but server is not enabled, run with -https-enabled")
+			continue
+		}
+		http.HandleFunc(path, Register(NewReverseProxy(u.Scheme, u.Host)))
+	}
+
+	if *httpsEnabled {
+		go func() {
+			// allow you to use self signed certificates
+			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+			// openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
+			log.Printf("start https server on %s", *httpsAddress)
+			if err := http.ListenAndServeTLS(*httpsAddress, filepath.Join(folder, "server.crt"), filepath.Join(folder, "server.key"), nil); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+	}
+
+	log.Printf("start http server on %s", *httpAddress)
+	if err := http.ListenAndServe(*httpAddress, nil); err != nil {
+		log.Fatalln(err)
 	}
 }
